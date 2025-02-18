6cc440b2b4a80e0eff57c3ee5008558ce259714b
Jeferson Pinheiro de Jesus <65078428+jeffpine@users.noreply.github.com>
2025-02-18 12:36:51 -0300

    Atualizar o authMiddleware.ts
    resolução de problemas na auth

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,29 +1,30 @@
-import {Request, Response, NextFunction } from "express";
-import connection from "../database/connection";
+import { Request, Response, NextFunction } from 'express';
+import connection from '../database/connection';
 
 export const authMiddleware = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
-    const userId = req.headers["user-id"];
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const userId = req.headers['user-id'];
 
-    if (!userId) {
-        res.status(401).json({ message: "Usuário não autenticado" });
+  if (!userId) {
+    return res.status(401).json({ message: 'Usuário não autenticado' });
+  }
+
+  try {
+    const [users] = await connection.execute(
+      'SELECT * FROM users WHERE id = ?',
+      [userId]
+    );
+
+    if (Array.isArray(users) && users.length === 0) {
+      return res.status(401).json({ message: 'Usuário não encontrado' });
     }
 
-    try {
-        const [users] = await connection.execute(
-            "SELECT * FROM users WHERE id = ?",
-            [userId]
-        );
-        
-        if (Array.isArray(users) && users.length === 0) {
-            res.status(401).json({ message: "Usuário não encontrado" });
-        }
-
-        next();
-    } catch (error) {
-        res.status(500).json({ message: "Erro ao verificar usuário" });
-    }
+    next(); // Chama next() para passar o controle para o próximo middleware
+  } catch (error) {
+    console.error('Erro ao verificar autenticação:', error);
+    return res.status(500).json({ message: 'Erro ao verificar autenticação' });
+  }
 };
